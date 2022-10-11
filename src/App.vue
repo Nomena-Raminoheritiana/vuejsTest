d473b5f1a5d6b4f8697defba0a37203a70f2f847
Nomena <n.raminoheritiana@bolzanogroup.com>
2022-10-11 08:54:41 +0300

    delete comments

--- src/App.vue
+++ src/App.vue
@@ -18,20 +18,6 @@ import {userInitialState} from './components/constants/initialState'
      <ListUsers v-bind:users="users" @suppression_user="deleteUser" @modification_user="updateUser"/>
     </div>
   </div>
-
-
-<!--  <h1 @click="increments">{{ count }}</h1>-->
-<!--  <header>-->
-<!--    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
-
-<!--    <div class="wrapper">-->
-<!--      <HelloWorld msg="You did it!" />-->
-<!--    </div>-->
-<!--  </header>-->
-
-<!--  <main>-->
-<!--    <TheWelcome />-->
-<!--  </main>-->
 </template>
 
 <style scoped>
